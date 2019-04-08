fa10fbcc14ec5b5a116907dec7a120d3687f334b
krzysztofserocki <leicer141@gmail.com>
2019-04-08 18:27:06 +0200

    implement XmlPullParser

--- app/src/main/java/com/leicer/top10apps/ParseApplication.java
+++ app/src/main/java/com/leicer/top10apps/ParseApplication.java
@@ -1,5 +1,9 @@
 package com.leicer.top10apps;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.StringReader;
 import java.util.ArrayList;
 
 public class ParseApplication {
@@ -13,5 +17,31 @@ public class ParseApplication {
 
     public ArrayList<FeedEntry> getApplications() {
         return applications;
+    }
+
+    public boolean parse(String xmlData) {
+        boolean status = true;
+        FeedEntry currentRecord;
+        boolean inEntry = false;
+        String textValue = "";
+
+        try {
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlPullParser xpp = factory.newPullParser();
+
+            factory.setNamespaceAware(true);
+            xpp.setInput(new StringReader(xmlData));
+
+            int eventType = xpp.getEventType();
+
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+
+            }
+        } catch (Exception e) {
+            status = false;
+            e.printStackTrace();
+        }
+
+        return status;
     }
 }
