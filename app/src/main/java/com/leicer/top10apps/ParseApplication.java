d4a419293b4d969a715f25c8605d695aeae36e6a
krzysztofserocki <leicer141@gmail.com>
2019-04-08 18:43:40 +0200

    parsing loop

--- app/src/main/java/com/leicer/top10apps/ParseApplication.java
+++ app/src/main/java/com/leicer/top10apps/ParseApplication.java
@@ -1,5 +1,7 @@
 package com.leicer.top10apps;
 
+import android.util.Log;
+
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserFactory;
 
@@ -21,7 +23,7 @@ public class ParseApplication {
 
     public boolean parse(String xmlData) {
         boolean status = true;
-        FeedEntry currentRecord;
+        FeedEntry currentRecord = new FeedEntry();
         boolean inEntry = false;
         String textValue = "";
 
@@ -35,6 +37,41 @@ public class ParseApplication {
             int eventType = xpp.getEventType();
 
             while (eventType != XmlPullParser.END_DOCUMENT) {
+                String tagName = xpp.getName();
+                switch (eventType) {
+                    case XmlPullParser.START_TAG:
+                        Log.d(TAG, "parse: Starting tag for " + tagName);
+                        if ("entry".equalsIgnoreCase(tagName)){
+                            inEntry = true;
+                            currentRecord = new FeedEntry();
+                        }
+                        break;
+                    case XmlPullParser.TEXT:
+                        textValue = xpp.getText();
+                        break;
+                    case XmlPullParser.END_TAG:
+                        Log.d(TAG, "parse: Ending tag for " + tagName);
+                        if (inEntry) {
+                            if ("entry".equalsIgnoreCase(tagName)) {
+                                applications.add(currentRecord);
+                                inEntry = false;
+                            } else if ("name".equalsIgnoreCase(tagName)) {
+                                currentRecord.setName(textValue);
+                            } else if ("artist".equalsIgnoreCase(tagName)) {
+                                currentRecord.setArtist(textValue);
+                            } else if ("releaseDate".equalsIgnoreCase(tagName)) {
+                                currentRecord.setReleaseDate(textValue);
+                            } else if ("summary".equalsIgnoreCase(tagName)) {
+                                currentRecord.setSummary(textValue);
+                            } else if ("image".equalsIgnoreCase(tagName)) {
+                                currentRecord.setImageurl(textValue);
+                            }
+                        }
+                        break;
+                        default:
+                            //Nothing else to do...
+                }
+                eventType = xpp.next();
 
             }
         } catch (Exception e) {
